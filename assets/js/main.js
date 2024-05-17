bbc95b9e6d6db34f8e420717075e5b56d524635a
jibinshan <jibinshanu23@gmail.com>
2024-05-17 12:32:31 +0530

    added faq for frontend

--- assets/js/main.js
+++ assets/js/main.js
@@ -399,3 +399,12 @@ document.addEventListener("DOMContentLoaded", function() {
 	  }
 	});
   });
+
+  //faq
+  const faq = document.querySelectorAll(".faq");
+
+  faq.forEach(faq =>{
+	faq.addEventListener('click',()=>{
+		faq.classList.toggle("active")
+	})
+  })
